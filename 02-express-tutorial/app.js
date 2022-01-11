7cd1158b0a0e14cf6da5337d30785254513da184
ellijs <ellijs@hotmail.com>
2022-01-11 01:49:09 -0500

    url

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -3,9 +3,29 @@ const http = require('http')
 
 // creating server
 const server = http.createServer((req, res) => {
-    res.writeHead(200, {'Content-type' : 'text/html'})
-    res.write('<h1>home page</h1>')
-    res.end()
+    // console.log(req.method)// => GET 
+    // console.log(req.url) // => '/'
+
+    const url = req.url;
+
+    // Home Page
+    if ( url === '/') {
+        res.writeHead(200, {'Content-type' : 'text/html'})
+        res.write('<h1>home page</h1>')
+        res.end()
+    }
+    // About Page 
+    else if ( url === '/about') {
+        res.writeHead(200, {'Content-type' : 'text/html'})
+        res.write('<h1>About page</h1>')
+        res.end()
+    } 
+    // 404
+    else {
+        res.writeHead(404, {'Content-type' : 'text/html'})
+        res.write('<h1>Page Not Found</h1>')
+        res.end()
+    }
 })
 
 // setting up port 
