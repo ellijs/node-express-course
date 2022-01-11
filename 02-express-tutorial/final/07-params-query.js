9f3c18e3348b642fa8803477fd10cf9198695265
ellijs <ellijs@hotmail.com>
2022-01-11 13:05:09 -0500

    api2

--- 02-express-tutorial/final/07-params-query.js
+++ 02-express-tutorial/final/07-params-query.js
@@ -6,7 +6,9 @@ const { products } = require('./data')
 app.get('/', (req, res) => {
   res.send('<h1> Home Page</h1><a href="/api/products">products</a>')
 })
+
 app.get('/api/products', (req, res) => {
+  // can be selective from data
   const newProducts = products.map((product) => {
     const { id, name, image } = product
     return { id, name, image }
