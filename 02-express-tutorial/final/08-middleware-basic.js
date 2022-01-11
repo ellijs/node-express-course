e8f3bdd832a8aefb4531bb34d394df85259ede26
ellijs <ellijs@hotmail.com>
2022-01-11 14:00:15 -0500

    next

--- 02-express-tutorial/final/08-middleware-basic.js
+++ 02-express-tutorial/final/08-middleware-basic.js
@@ -8,6 +8,7 @@ const logger = (req, res, next) => {
   const url = req.url
   const time = new Date().getFullYear()
   console.log(method, url, time)   //=> GET / 2022
+  // you MUST pass th next to pass to next middleware!!!! or sending the data
   next()
 }
 
