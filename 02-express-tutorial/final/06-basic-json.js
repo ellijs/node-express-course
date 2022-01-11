6843b417ad0639c53dfd279936105454ba7b797b
ellijs <ellijs@hotmail.com>
2022-01-11 02:55:42 -0500

    data

--- 02-express-tutorial/final/06-basic-json.js
+++ 02-express-tutorial/final/06-basic-json.js
@@ -8,3 +8,8 @@ app.get('/', (req, res) => {
 app.listen(5000, () => {
   console.log('Server is listening on port 5000....')
 })
+
+
+// app.get('/', (req, res) => {
+//   res.json([{name: "Susan"}, {name: "Bob"}])
+// })
