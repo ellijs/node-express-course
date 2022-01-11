85fd3dae1fce2ddad3af011ff04a5badeddb5283
ellijs <ellijs@hotmail.com>
2022-01-11 02:07:18 -0500

    adding files to browser

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -6,7 +6,10 @@ const { readFileSync } = require('fs')
 
 
 // get all files
-const homePage = readFileSync('./index.html')
+const homePage = readFileSync('./navbar-app/index.html')
+const homeStyles = readFileSync('./navbar-app/styles.css')
+const homeImage = readFileSync('./navbar-app/logo.svg')
+const homeLogic = readFileSync('./navbar-app/browser-app.js')
 
 
 // creating server
@@ -15,6 +18,7 @@ const server = http.createServer((req, res) => {
     // console.log(req.url) // => '/'
 
     const url = req.url;
+    console.log(url)
 
     // Home Page
     if ( url === '/') {
@@ -29,6 +33,24 @@ const server = http.createServer((req, res) => {
         res.write('<h1>About page</h1>')
         res.end()
     } 
+    // Styles
+    else if ( url === '/styles.css') {
+        res.writeHead(200, {'Content-type' : 'text/css'})
+        res.write(homeStyles)
+        res.end()
+    }
+    // image
+    else if ( url === '/logo.svg') {
+        res.writeHead(200, {'Content-type' : 'image/svg+xml'})
+        res.write(homeImage)
+        res.end()
+    }
+    // logic
+    else if ( url === '/browser-app.js') {
+        res.writeHead(200, {'Content-type' : 'text/javascript'})
+        res.write(homeLogic)
+        res.end()
+    }
     // 404
     else {
         res.writeHead(404, {'Content-type' : 'text/html'})
