b09b0c882bef56778ef76150a0d556b98472a5ac
ellijs <ellijs@hotmail.com>
2022-01-11 01:32:31 -0500

    http

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,8 +1,11 @@
-
+// use http module
 const http = require('http')
 
+// creating server
 const server = http.createServer((req, res) => {
     console.log('user hit the server')
+    res.end('home page')
 })
 
+// setting up port 
 server.listen(5000)
