3b5d89decccabd4d41ef9adffc68647057b93b5e
ellijs <ellijs@hotmail.com>
2022-01-11 17:45:35 -0500

    All Done with nodejs

--- 02-express-tutorial/routes/people.js
+++ 02-express-tutorial/routes/people.js
@@ -65,3 +65,32 @@ router.get('/', (req, res) => {
 
 
   module.exports = router
+
+
+
+//   It's better to make it seperate from Controller and Router
+
+
+// const express = require('express')
+// const router = express.Router()
+
+// const {
+//   getPeople,
+//   createPerson,
+//   createPersonPostman,
+//   updatePerson,
+//   deletePerson,
+// } = require('../controllers/people')
+
+// // router.get('/', getPeople)
+// // router.post('/', createPerson)
+// // router.post('/postman', createPersonPostman)
+// // router.put('/:id', updatePerson)
+// // router.delete('/:id', deletePerson)
+
+// // can chain one after another
+// router.route('/').get(getPeople).post(createPerson)
+// router.route('/postman').post(createPersonPostman)
+// router.route('/:id').put(updatePerson).delete(deletePerson)
+
+// module.exports = router
