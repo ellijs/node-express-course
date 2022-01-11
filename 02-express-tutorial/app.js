a1c3d8ad4495b7c3c54639bc933eef8e343e8ee1
ellijs <ellijs@hotmail.com>
2022-01-11 01:36:16 -0500

    server

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -3,8 +3,9 @@ const http = require('http')
 
 // creating server
 const server = http.createServer((req, res) => {
-    console.log('user hit the server')
-    res.end('home page')
+    res.writeHead(200, {'Content-type' : 'text/html'})
+    res.write('<h1>home page</h1>')
+    res.end()
 })
 
 // setting up port 
