f0992dea00001b038f7f0e40ba15e58194d76e03
ellijs <ellijs@hotmail.com>
2022-01-11 17:11:43 -0500

    controller

--- 02-express-tutorial/final/11-methods.js
+++ 02-express-tutorial/final/11-methods.js
@@ -2,9 +2,17 @@ const express = require('express')
 const app = express()
 let { people } = require('./data')
 
+// const people = [
+//   { id: 1, name: 'john' },
+//   { id: 2, name: 'peter' },
+//   { id: 3, name: 'susan' },
+//   { id: 4, name: 'anna' },
+//   { id: 5, name: 'emma' },
+// ]
+
 // static assets
 app.use(express.static('./methods-public'))
-// parse form data
+// parse form data => in order to get form data
 app.use(express.urlencoded({ extended: false }))
 // parse json
 app.use(express.json())
@@ -14,7 +22,7 @@ app.get('/api/people', (req, res) => {
 })
 
 app.post('/api/people', (req, res) => {
-  const { name } = req.body
+  const { name } = req.body  // => { name: "Jessica" }
   if (!name) {
     return res
       .status(400)
@@ -43,7 +51,9 @@ app.post('/login', (req, res) => {
 })
 
 app.put('/api/people/:id', (req, res) => {
+
   const { id } = req.params
+  
   const { name } = req.body
 
   const person = people.find((person) => person.id === Number(id))
