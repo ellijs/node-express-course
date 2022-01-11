03d3f521cd8351ddbae4bbc8e9b2282813160469
ellijs <ellijs@hotmail.com>
2022-01-11 14:20:41 -0500

    authorize

--- 02-express-tutorial/final/09-middleware-use.js
+++ 02-express-tutorial/final/09-middleware-use.js
@@ -7,7 +7,8 @@ const authorize = require('./authorize')
 // app.use only works 이것보다 아래에 있는것만.
 // app.use('/api', logger) => 'use' invoke this to any '/api' route
 
-app.use([logger, authorize])
+app.use([logger, authorize]) // execute multiple middleware function [] // 순서에 따라 실행된다. 
+
 // api/home/about/products
 app.get('/', (req, res) => {
   res.send('Home')
