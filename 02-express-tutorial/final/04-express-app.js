7b090bf51c47b344e11e9753767cc98f63360fb8
ellijs <ellijs@hotmail.com>
2022-01-11 02:41:37 -0500

    static git push

--- 02-express-tutorial/final/04-express-app.js
+++ 02-express-tutorial/final/04-express-app.js
@@ -8,9 +8,12 @@ const app = express()
 // server doesn't need to change, express will take care of everything
 app.use(express.static('./public'))
 
-app.get('/', (req, res) => {
-  res.sendFile(path.resolve(__dirname, './navbar-app/index.html'))
-})
+// app.get('/', (req, res) => {
+//   res.sendFile(path.resolve(__dirname, './navbar-app/index.html'))
+// })
+// sendFile option when to use
+// => adding to static assets 
+// SSR
 
 app.all('*', (req, res) => {
   res.status(404).send('resource not found')
